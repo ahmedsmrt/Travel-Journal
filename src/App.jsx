312967f4a975f06a8cd52cb2c900bc179f0551f7
ahmedsmrt <ahmedsmrt@gmail.com>
2022-07-22 18:05:41 -0400

    Header Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div className="App">
-     
-    </div>
+    <main>
+      <Header />
+    </main>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav>
+      <div className="nav-wrap">
+        <img src="../assets/globe.svg" alt="travel logo" />
+        <h3>my travel journal.</h3>
+      </div>
+    </nav>
+  );
+}
